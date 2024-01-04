a5212932db554576a50a4e5889a10ccca73f2b0e
Jean-Paul <jean.paul.bassil@outlook.com>
2024-01-04 15:50:15 +0200

    Cleanup: created FontLoader Hook and separate styled component

---
+++ components/styles/LoginScreenStyles.ts
@@ -0,0 +1,29 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'flex-start',
+    padding: 20,
+    paddingTop: '20%',
+  },
+  title: {
+    marginTop: 16,
+    fontSize: 26,
+    marginBottom: 8,
+    color: '#121420',
+  },
+  scanner: {
+    color: '#005C4A',
+  },
+  subtitle: {
+    marginTop: 50,
+    fontSize: 16,
+    marginBottom: 90,
+    color: '#AAAAAA',
+  },
+  input: {
+    marginBottom: 40,
+    paddingBottom: 15,
+  },
+});
--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -1,39 +1,29 @@
-import * as React from 'react';
+import React from 'react';
+import { View, Text } from 'react-native';
 import Button from '../components/Button';
 import ImageLogo from '../components/ImageLogo';
 import InputField from '../components/InputField';
-import { View, StyleSheet, Text, Image, TouchableOpacity } from 'react-native';
-import { useFonts, PTSans_400Regular, PTSans_700Bold } from '@expo-google-fonts/pt-sans';
+import useFontLoader from '../hooks/useFontLoader';
 import AppLoading from 'expo-app-loading';
-import * as Font from 'expo-font';
-import { TextInput } from 'react-native-gesture-handler';
-import Icon from 'react-native-vector-icons/FontAwesome';
-
+import styles from '../components/styles/LoginScreenStyles';
 
 const LoginScreen = () => {
   const [username, setUsername] = React.useState<string>('');
   const [password, setPassword] = React.useState<string>('');
-  const [isReady, setIsReady] = React.useState(false);
 
-  React.useEffect(() => {
-    async function loadFonts() {
-      await Font.loadAsync({
-        PTSans_400Regular,
-        PTSans_700Bold,
-      });
-      setIsReady(true);
-    }
+  const fontsLoaded = useFontLoader();
 
-    loadFonts();
-  }, []);
-
-  if (!isReady) {
+  if (!fontsLoaded) {
     return <AppLoading />;
   }
 
+  const handlePress = () => {
+    console.log('Button pressed with username:', username, 'and password:', password);
+  };
+
   return (
     <View style={styles.container}>
-      <ImageLogo/>
+      <ImageLogo />
       <Text style={styles.title}>
         Welcome to LAU {'\n'}Barcode{' '}
         <Text style={styles.scanner}>scanner!</Text>
@@ -54,42 +44,9 @@ const LoginScreen = () => {
         secureTextEntry
         style={styles.input}
       />
-      <Button onPress={handlePress} title="→" style={styles.button} textStyle={styles.buttonText} />
+      <Button onPress={handlePress} title="→"/>
     </View>
   );
 };
-
-function handlePress() {
-  console.log('Button pressed');
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'flex-start',
-    padding: 20,
-    paddingTop: '20%', 
-    fontFamily: 'PTSans_400Regular',
-  },
-  title: {
-    marginTop: 16,
-    fontSize: 26,
-    marginBottom: 8,
-    color: '#121420'
-  },
-  scanner: {
-    color: '#005C4A',
-  },
-  subtitle: {
-    marginTop: 50,
-    fontSize: 16,
-    marginBottom: 90,
-    color: '#AAAAAA'
-  },
-  input: {
-    marginBottom: 40,
-    paddingBottom: 15,
-  },
-});
 
 export default LoginScreen;
