9bbb20be01909547477ebec81cbbc87d37b3b07a
JayPee <jean.paul.bassil@outlook.com>
2024-01-17 18:11:04 +0200

    modify: created useCamera Hook

--- screens/events/Event.tsx
+++ screens/events/Event.tsx
@@ -5,8 +5,7 @@ import Constants from 'expo-constants';
 import ErrorDisplay from '../../components/ErrorDisplay';
 import Button from '../../components/Button';
 import { BarCodeScanner } from 'expo-barcode-scanner';
-import { Camera, CameraType } from 'expo-camera';
-import { FlashMode } from 'react-native-camera';
+import { Camera, AutoFocus } from 'expo-camera';
 import { MaterialIcons } from '@expo/vector-icons';
 import CameraButton from '../../components/CameraButtons';
 import api from '../../services/api';
@@ -15,33 +14,29 @@ import StudentItem from '../../components/StudentItem';
 import { IStudent } from '../../interfaces/students.interface';
 import useForm from '../../hooks/useForm';
 import useModal from '../../hooks/useModal';
+import useCamera from '../../hooks/useCamera';
 
 const EventDetailScreen = ({ route }: any) => {
     const { event } = route.params;
     const [loading, setLoading] = useState(false);
     const [students, setStudents] = useState([]);
     const [scanData, setScanData] = useState(null);
-    const [isCameraVisible, setIsCameraVisible] = useState(false);
     const [error, setError] = useState('');
-    const [hasPermission, setHasPermission] = useState<boolean>(false);
-    const [data, setData] = useState<string>('');
-    const [type, setType] = useState<CameraType>(Camera.Constants.Type.back);
-    const [flash, setFlash] = useState<FlashMode>(Camera.Constants.FlashMode.off);
     const [showNameModal, setShowNameModal] = useState<boolean>(false);
     const cameraRef = useRef<Camera>(null);
     const [currentStudentId, setCurrentStudentId] = useState<number | null>(null);
     const [newName, setNewName] = useState('');
     const { values: formValues, handleChange, resetForm } = useForm({ studentName: '' });
-
     const cameraModal = useModal();
     const editModal = useModal();
     const nameModal = useModal();
+    const { hasPermission, type, toggleCameraType, flashMode, toggleFlashMode, isCameraVisible, openCameraModal, closeCameraModal } = useCamera();
 
 
     const handleStudentNameSubmit = async () => {
         try {
             const studentData = { student_id: parseInt(data), name: formValues.studentName };
-            const response = await api.addStu dent(studentData, event._id);
+            const response = await api.addStudent(studentData, event._id);
     
             resetForm(); 
             nameModal.closeModal(); 
@@ -55,7 +50,6 @@ const EventDetailScreen = ({ route }: any) => {
         const fetchStudents = async () => {
             try {
                 const response = await api.getEventAttendees(event._id);
-                console.log(response.data.attendees)
                 setStudents(response.data.attendees);
             } catch (error) {
                 console.error('Error fetching students:', error);
@@ -65,12 +59,7 @@ const EventDetailScreen = ({ route }: any) => {
         fetchStudents();
     }, []);
 
-    useEffect(() => {
-        (async () => {
-            const { status } = await Camera.requestCameraPermissionsAsync();
-            setHasPermission(status === 'granted');
-        })();
-    }, []);
+    
     
 
 
@@ -83,7 +72,7 @@ const EventDetailScreen = ({ route }: any) => {
         try {
             const response = await api.editStudent(currentStudentId, newName);    
             editModal.closeModal(); 
-        } catch (error) {
+        } catch (error: any) {
             console.error('Error editing student:', error);
             setError(error.response.data.message);
         }
@@ -121,30 +110,28 @@ const EventDetailScreen = ({ route }: any) => {
                 <Camera
                     style={cameraStyles.camera}
                     type={type}
-                    flashMode={flash}
+                    flashMode={flashMode}
                     ref={cameraRef}
-                    autoFocus={Camera.Constants.AutoFocus.on}
+                    autoFocus={AutoFocus.on}
                     onBarCodeScanned={ async (scan) => {
-                            setData(scan.data);
-                            console.log(scan.data);
-                            cameraModal.closeModal();
-                    
-                            try {
-                                const studentData = { student_id: parseInt(scan.data) };
-                                console.log(studentData.id, typeof studentData.id)
-                                const response = await api.addStudent(studentData, event._id);
-                                console.log('Student added:', response);
-                            } catch (error: any) {
-                                console.error('Error adding student:', error.response.data.message);
-                                const regex = /^Student with ID \d{9} not found, Please provide a name$/;
-                                if(regex.test(error.response.data.message)){
-                                    console.log("Here")
-                                    setShowNameModal(true);
-                                } else {
-                                    setError(error.response.data.message);
-                                }
-                            } 
-                    
+                        setScanData(scan.data);
+                        console.log(scan.data);
+                        cameraModal.closeModal();
+                        try {
+                            const studentData = { student_id: parseInt(scan.data) };
+                            console.log(studentData.id, typeof studentData.id)
+                            const response = await api.addStudent(studentData, event._id);
+                            console.log('Student added:', response);
+                        } catch (error: any) {
+                            console.error('Error adding student:', error.response.data.message);
+                            const regex = /^Student with ID \d{9} not found, Please provide a name$/;
+                            if(regex.test(error.response.data.message)){
+                                console.log("Here")
+                                setShowNameModal(true);
+                            } else {
+                                setError(error.response.data.message);
+                            }
+                        } 
                     }}
                 >
                     
@@ -155,16 +142,9 @@ const EventDetailScreen = ({ route }: any) => {
                         padding: 20,
                         backgroundColor: 'black',
                     }}>
-                        <CameraButton icon={'retweet'} onPress={() => {
-                            setType(type === Camera.Constants.Type.back ? Camera.Constants.Type.front : Camera.Constants.Type.back)
-                        }}/>
-                        <CameraButton icon={'flash'} onPress={() => {
-                            setFlash(flash === Camera.Constants.FlashMode.off ? Camera.Constants.FlashMode.torch : Camera.Constants.FlashMode.off)
-                        }}/>
-                        <CameraButton icon={'close'} onPress={() => {
-                            setIsCameraVisible(false);
-                            setData('');
-                        }}/>
+                        <CameraButton icon={'retweet'} onPress={toggleCameraType}/>
+                        <CameraButton icon={'flash'} onPress={toggleFlashMode}/>
+                        <CameraButton icon={'close'} onPress={cameraModal.closeModal}/>
                     </View>
             </View>}
             <StudentNameModal
@@ -184,7 +164,7 @@ const EventDetailScreen = ({ route }: any) => {
         <View style={styles.eventsList}>
             <FlatList
                 data={students}
-                keyExtractor={(item) => item._id} 
+                keyExtractor={(item: any) => item._id} 
                 renderItem={renderStudentItem}
             />
         </View>
