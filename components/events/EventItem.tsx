ab3736010cbac9d5a1ec4af995621c41a4c1bac3
tonyykosseifyy <tonykosseify123@gmail.com>
2024-01-21 01:07:01 +0200

    add: disabling events when inactive

--- components/events/EventItem.tsx
+++ components/events/EventItem.tsx
@@ -1,21 +1,27 @@
-import React from 'react';
-import { Text, TouchableOpacity, View } from 'react-native';
-import styles from '../../styles/home/event-item';
-import { Fontisto } from '@expo/vector-icons';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
-import { IEvent } from '../../interfaces/events.interface';
-import { Ionicons } from '@expo/vector-icons';
+import React from "react";
+import { Text, TouchableOpacity, View } from "react-native";
+import styles from "../../styles/home/event-item";
+import { Fontisto } from "@expo/vector-icons";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { IEvent } from "../../interfaces/events.interface";
+import { Ionicons } from "@expo/vector-icons";
 
 interface EventItemProps {
   event: IEvent;
   onPress: () => void;
 }
+const isActive = (event: IEvent): boolean => {
+  const currentDate = new Date();
+  const eventDate = new Date(event.start_date);
+  return eventDate < currentDate;
+};
+
 const getTime = (date: Date): string => {
   const hours = date.getHours();
   const minutes = date.getMinutes();
 
-  const formattedHours = hours.toString().padStart(2, '0');
-  const formattedMinutes = minutes.toString().padStart(2, '0');
+  const formattedHours = hours.toString().padStart(2, "0");
+  const formattedMinutes = minutes.toString().padStart(2, "0");
 
   return `${formattedHours}:${formattedMinutes}`;
 };
@@ -27,27 +33,37 @@ const EventItem: React.FC<EventItemProps> = ({ event, onPress }) => {
   console.log(event);
 
   return (
-    <TouchableOpacity style={styles.event_container} onPress={onPress}>
-      <View>
+    <TouchableOpacity
+      style={styles.event_container}
+      onPress={onPress}
+      disabled={!isActive(event)}
+    >
+      <View style={styles.event_left}>
         <Text style={styles.event_name}>{event.name}</Text>
         <View>
-        <View style={[styles.event_detail, { marginTop: 13 }]}>
-          <Fontisto name="date" size={14} color="#005C4A" />
-            <Text style={styles.event_detail_text}>{new Date(event.start_date).toDateString()}</Text>
-          </View>    
+          <View style={[styles.event_detail, { marginTop: 13 }]}>
+            <Fontisto name="date" size={14} color="#005C4A" />
+            <Text style={styles.event_detail_text}>
+              {new Date(event.start_date).toDateString()}
+            </Text>
+          </View>
           <View style={styles.event_detail}>
-            <MaterialCommunityIcons name="clock" size={14} color="#005C4A" />   
-            <Text style={styles.event_detail_text}>{getTime(new Date(event.start_date))} - {getTime(new Date(event.end_date))}</Text>
-          </View>  
+            <MaterialCommunityIcons name="clock" size={14} color="#005C4A" />
+            <Text style={styles.event_detail_text}>
+              {getTime(new Date(event.start_date))} -{" "}
+              {getTime(new Date(event.end_date))}
+            </Text>
+          </View>
           <View style={styles.event_detail}>
             <Ionicons name="person" size={14} color="#005C4A" />
             <Text style={styles.event_detail_text}>30 attendees</Text>
-
-          </View> 
+          </View>
         </View>
       </View>
 
-      <Text style={styles.event_arrow}>→</Text>
+      <View style={[styles.event_arrow_container, !isActive(event) && styles.event_arrow_container_inactive]}>
+        <Text style={styles.event_arrow}>→</Text>
+      </View>
     </TouchableOpacity>
   );
 };
--- styles/home/event-item.ts
+++ styles/home/event-item.ts
@@ -6,13 +6,16 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         backgroundColor: '#FFF',
         borderRadius: 10,
-        padding: 15,
         marginTop: 10,
         marginHorizontal: 15,
         justifyContent: 'space-between',
         shadowColor: 'black',
         shadowOpacity: 0.26,
         shadowOffset: { width: 0, height: 2},
+        shadowRadius: 10,
+    },
+    event_left: {
+        padding: 15,
     },
     event_name: {
         fontSize: 20,
@@ -27,9 +30,22 @@ const styles = StyleSheet.create({
         fontSize: 14,
         marginLeft: 10,
     },
+    event_arrow_container: {
+        backgroundColor: '#005C4A',
+        height: '100%',
+        display: "flex",
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: 60,
+        borderTopRightRadius: 10,
+        borderBottomRightRadius: 10,
+    },
+    event_arrow_container_inactive: {
+        opacity: 0.5,
+    },
     event_arrow: {
         fontSize: 20,
-        color: '#005C4A',
+        color: 'white',
     },
     
 });
