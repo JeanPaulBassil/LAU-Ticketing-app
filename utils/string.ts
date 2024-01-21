53d0c9b73c544c36d55cf6cd04e607161b730376
tonyykosseifyy <tonykosseify123@gmail.com>
2024-01-21 02:39:09 +0200

    fix: capitalize function to keep all non-first letters the same

--- utils/string.ts
+++ utils/string.ts
@@ -1,10 +1,8 @@
 export const capitalize = (str: string | undefined): string => {
     if (!str) return "";
-    return str
-      .split(" ")
-      .map((word) => {
-        return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
-      })
-      .join(" ");
+    return str.split(' ').map(word => {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    }).join(' ');
+  
   };
   
