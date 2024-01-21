1e7e51233afcfda336c793de50d57a812d2327e9
tonyykosseifyy <tonykosseify123@gmail.com>
2024-01-21 16:36:32 +0200

    styling: bottom navigation

---
+++ screens/settings/Settings.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text, TouchableOpacity, ActivityIndicator } from "react-native";
+
+
+const Settings = () => {
+    return (
+        <View>
+            <Text>Settings</Text>
+        </View>
+    )
+}
+
+export default Settings;
