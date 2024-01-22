9ecb5c080a22752538a2a92c1bbbf56f87b06ea1
JayPee <jean.paul.bassil@outlook.com>
2024-01-22 15:02:33 +0200

    modify: fixed the error to use the context

--- hooks/useEvents.ts
+++ hooks/useEvents.ts
@@ -1,45 +1,47 @@
-import { useEffect, useState, useContext } from "react";
+import { useEffect, useContext, useState } from "react";
 import api from "../services/api";
 import { IEvent, createEventData } from "../interfaces/events.interface";
 import { AxiosResponse } from "axios";
 import { EventDetailContext } from "../contexts/EventDetails";
 
-
 const useEvents = () => {
-    const [loading, setLoading] = useState<boolean>(false);
     const { eventState, dispatch } = useContext(EventDetailContext);
-    const events = eventState.events;
+    const { events, error } = eventState;  // Get error from eventState
+    const [loading, setLoading] = useState<boolean>(false);
 
     const fetchEvents = async () => {
         try {
             setLoading(true);
             const response: AxiosResponse = await api.getEvents();
             dispatch({ type: 'SET_EVENTS', payload: response.data });
+            dispatch({ type: 'SET_ERROR', payload: '' }); // Clear any existing error
         } catch (error: any) {
             dispatch({ type: 'SET_ERROR', payload: error.response?.data?.message || "An error occurred while fetching events." });
         } finally {
             setLoading(false);
         }
-    }  
-    
+    };
+
     useEffect(() => {
-        fetchEvents();
+        if (events.length === 0) {  // Fetch events only if they are not already fetched
+            fetchEvents();
+        }
     }, []);
 
     const addEvent = async (data: createEventData) => {
         try {
             setLoading(true);
             await api.createEvent(data);
-            await fetchEvents();
-            dispatch({ type: 'SET_ERROR', payload: '' })
+            await fetchEvents();  // Re-fetch events to update the list
         } catch (error: any){
             dispatch({ type: 'SET_ERROR', payload: error.response?.data?.message || "An error occurred while adding event." });
         } finally {
             setLoading(false);
         }
-    }
+    };
 
-    return { events, loading, fetchEvents, addEvent };
+    return { events, loading, fetchEvents, addEvent, error };
+
 }
 
 export default useEvents;
