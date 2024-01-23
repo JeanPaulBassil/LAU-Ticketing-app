2f60aa0581a4a220dca1aa4c94a36f3cc1de90c7
tonyykosseifyy <tonykosseify123@gmail.com>
2024-01-23 13:35:55 +0200

    fix: scrolling issues

--- components/events/EventList.tsx
+++ components/events/EventList.tsx
@@ -1,8 +1,7 @@
 import React, { useState, useCallback } from 'react';
-import { FlatList, View } from 'react-native';
+import { ScrollView, View, RefreshControl, StyleSheet } from 'react-native';
 import EventItem from './EventItem';
 import { IEvent } from '../../interfaces/events.interface';
-import { StyleSheet, RefreshControl } from "react-native";
 import useEvents from '../../hooks/useEvents';
 
 interface EventListProps {
@@ -18,52 +17,41 @@ const EventList = ({ events, navigation, error, loading }: EventListProps) => {
 
     const onRefresh = useCallback(async () => {
         setIsRefreshing(true);
-        console.log('refreshing')
         await fetchEvents();
         setIsRefreshing(false);
-        console.log('done refreshing')
     }, []);
-
-
 
     if (error || loading && !isRefreshing || Array.isArray(events) && events.length === 0) {
         return null;
     }
 
-    const renderItem = ({ item } : { item: IEvent } ) => {
-        return (
-        <EventItem 
-            event={item}
-            onPress={() => navigation.navigate('Event', { event: item })}
-        />
-    )};
-
     return (
-        <View style={styles.events_list}>
-            <FlatList
-                data={events}
-                keyExtractor={(event) => event._id}
-                renderItem={renderItem}
-                style={styles.events_list}
-                refreshControl={
-                    <RefreshControl
-                        refreshing={isRefreshing}
-                        onRefresh={onRefresh}
-                    />
-                }
-            />
-        </View>
+        <ScrollView
+            style={styles.events_list}
+            refreshControl={
+                <RefreshControl
+                    refreshing={isRefreshing}
+                    onRefresh={onRefresh}
+                />
+            }
+        >
+            {events.map((item: IEvent) => (
+                <EventItem 
+                    key={item._id}
+                    event={item}
+                    onPress={() => navigation.navigate('Event', { event: item })}
+                />
+            ))}
+        </ScrollView>
     );
 };
 
 export default EventList;
 
-
 const styles = StyleSheet.create({
     events_list: {
-        width: '100%',
-        height: '100%',
-        zIndex: -1,
+        flex: 1,
         paddingBottom: 60,
+        // backgroundColor: 'red', // Adjust as needed
     }
 });
--- styles/home/event-item.ts
+++ styles/home/event-item.ts
@@ -2,17 +2,19 @@ import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
     event_container: {
+        display: 'flex',
         flexDirection: 'row',
-        alignItems: 'center',
         backgroundColor: '#FFF',
         borderRadius: 10,
-        marginTop: 10,
         marginHorizontal: 15,
+        marginVertical: 8,
         justifyContent: 'space-between',
         shadowColor: 'black',
         shadowOpacity: 0.26,
         shadowOffset: { width: 0, height: 2},
         shadowRadius: 10,
+        height: 140,
+        elevation: 5, // Added for shadow effect on Android
     },
     event_left: {
         padding: 15,
